e8a800757b29795a24daee5b69455e3e3f2fc689
Michelle S Bachler <michelle.bachler@open.ac.uk>
2023-05-11 16:40:58 +0100

    Add new Google snippet for Analytics 4

--- ui/analyticstracking.php
+++ ui/analyticstracking.php
@@ -1,3 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php print($CFG->GOOGLE_SITE_TAG);?>"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php print($CFG->GOOGLE_SITE_TAG);?>');
+</script>
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),

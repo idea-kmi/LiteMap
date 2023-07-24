9ec799d442a7e227816d584d1a53c007cf63b0c8
msbachler <michelle.bachler@open.ac.uk>
2023-07-24 11:23:14 +0100

    Removing old unused file that wher in original LiteMap and are no longer used

--- core/lib/hybridauth/config.php
+++ core/lib/hybridauth/config.php
@@ -18,55 +18,64 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 global $CFG;
 
 $settings  = array(
-		"base_url" => $CFG->homeAddress.'core/lib/hybridauth/',
-		"providers" => array(
-			"Twitter" => array (
-				"enabled" => $CFG->SOCIAL_SIGNON_TWITTER_ON,
-				"keys"    => array ( "key" => $CFG->SOCIAL_SIGNON_TWITTER_ID, "secret" => $CFG->SOCIAL_SIGNON_TWITTER_SECRET ),
-				"includeEmail" => true
-			),
-			"Yahoo" => array (
-				"enabled" => $CFG->SOCIAL_SIGNON_YAHOO_ON,
-				"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_YAHOO_ID, "secret" => $CFG->SOCIAL_SIGNON_YAHOO_SECRET ),
-				"scope"   => array ('sdps-r')
-			),
-			"LinkedIn" => array (
-				"enabled" => $CFG->SOCIAL_SIGNON_LINKEDIN_ON,
-				"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_LINKEDIN_ID, "secret" => $CFG->SOCIAL_SIGNON_LINKEDIN_SECRET ),
-            	"fields" => array(),
-			),
-			"Google" => array (
-				"enabled" => $CFG->SOCIAL_SIGNON_GOOGLE_ON,
-				"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_GOOGLE_ID, "secret" => $CFG->SOCIAL_SIGNON_GOOGLE_SECRET )
-			),
-			"Facebook" => array (
-				"enabled" => $CFG->SOCIAL_SIGNON_FACEBOOK_ON,
-				"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_FACEBOOK_ID, "secret" => $CFG->SOCIAL_SIGNON_FACEBOOK_SECRET ),
-				"trustForwarded" => false
-			),
-			// windows live
-			"Live" => array(
-				"enabled" => false,
-				"keys" => array("id" => "", "secret" => ""),
-			),
-			"Foursquare" => array(
-				"enabled" => false,
-				"keys" => array("id" => "", "secret" => ""),
-			),
-			"OpenID" => array(
-				"enabled" => false,
-			),
-			"AOL" => array(
-				"enabled" => false,
-			),
+    //"base_url" => $CFG->homeAddress.'core/lib/hybridauth/',
+    "callback" => $CFG->homeAddress.'ui/pages/loginexternal.php',
+    //"callback" => Hybridauth\HttpClient\Util::getCurrentUrl(),
+    "providers" => array(
+		"Twitter" => array (
+			"enabled" => $CFG->SOCIAL_SIGNON_TWITTER_ON,
+			"keys"    => array ( "key" => $CFG->SOCIAL_SIGNON_TWITTER_ID, "secret" => $CFG->SOCIAL_SIGNON_TWITTER_SECRET ),
+			"includeEmail" => true
 		),
-		// If you want to enable logging, set 'debug_mode' to true.
-		// You can also set it to
-		// - "error" To log only error messages. Useful in production
-		// - "info" To log info and error messages (ignore debug messages)
-		"debug_mode" => false,
-		// Path to file writable by the web server. Required if 'debug_mode' is not false
-		"debug_file" => ""
+		"Yahoo" => array (
+			"enabled" => $CFG->SOCIAL_SIGNON_YAHOO_ON,
+			"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_YAHOO_ID, "secret" => $CFG->SOCIAL_SIGNON_YAHOO_SECRET ),
+			"scope"   => array ('sdps-r')
+		),
+		"LinkedIn" => array (
+			"enabled" => $CFG->SOCIAL_SIGNON_LINKEDIN_ON,
+			"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_LINKEDIN_ID, "secret" => $CFG->SOCIAL_SIGNON_LINKEDIN_SECRET ),
+			"fields" => array(),
+		),
+		"Google" => array (
+			"enabled" => $CFG->SOCIAL_SIGNON_GOOGLE_ON,
+			"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_GOOGLE_ID, "secret" => $CFG->SOCIAL_SIGNON_GOOGLE_SECRET )
+		),
+		"Facebook" => array (
+			"enabled" => $CFG->SOCIAL_SIGNON_FACEBOOK_ON,
+			"keys"    => array ( "id" => $CFG->SOCIAL_SIGNON_FACEBOOK_ID, "secret" => $CFG->SOCIAL_SIGNON_FACEBOOK_SECRET ),
+			"trustForwarded" => false
+		),
+
+        // openid providers
+        "OpenID" => array(
+            "enabled" => true,
+        ),
+        "AOL" => array(
+            "enabled" => true,
+        ),
+        // windows live
+        "Live" => array(
+            "enabled" => true,
+            "keys" => array("id" => "", "secret" => ""),
+        ),
+        "LinkedIn" => array(
+            "enabled" => true,
+            "keys" => array("id" => "", "secret" => ""),
+            "fields" => array(),
+        ),
+        "Foursquare" => array(
+            "enabled" => true,
+            "keys" => array("id" => "", "secret" => ""),
+        ),
+    ),
+    // If you want to enable logging, set 'debug_mode' to true.
+    // You can also set it to
+    // - "error" To log only error messages. Useful in production
+    // - "info" To log info and error messages (ignore debug messages)
+    "debug_mode" => false,
+    // Path to file writable by the web server. Required if 'debug_mode' is not false
+    "debug_file" => "",
 );
 
 if ($CFG->PROXY_HOST != "") {

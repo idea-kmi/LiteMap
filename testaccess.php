d1517e9915b5bce29d58708898e9810c4f286a0a
Michelle S Bachler <michelle.bachler@open.ac.uk>
2023-07-24 14:08:01 +0100

    Added back the testaccess.php - forgot the external monitoring sites use it

---
+++ testaccess.php
@@ -0,0 +1,45 @@
+<?php
+/********************************************************************************
+ *                                                                              *
+ *  (c) Copyright 2015 The Open University UK                                   *
+ *                                                                              *
+ *  This software is freely distributed in accordance with                      *
+ *  the GNU Lesser General Public (LGPL) license, version 3 or later            *
+ *  as published by the Free Software Foundation.                               *
+ *  For details see LGPL: http://www.fsf.org/licensing/licenses/lgpl.html       *
+ *               and GPL: http://www.fsf.org/licensing/licenses/gpl-3.0.html    *
+ *                                                                              *
+ *  This software is provided by the copyright holders and contributors "as is" *
+ *  and any express or implied warranties, including, but not limited to, the   *
+ *  implied warranties of merchantability and fitness for a particular purpose  *
+ *  are disclaimed. In no event shall the copyright owner or contributors be    *
+ *  liable for any direct, indirect, incidental, special, exemplary, or         *
+ *  consequential damages (including, but not limited to, procurement of        *
+ *  substitute goods or services; loss of use, data, or profits; or business    *
+ *  interruption) however caused and on any theory of liability, whether in     *
+ *  contract, strict liability, or tort (including negligence or otherwise)     *
+ *  arising in any way out of the use of this software, even if advised of the  *
+ *  possibility of such damage.                                                 *
+ *                                                                              *
+ ********************************************************************************/
+    include_once("config.php");
+	$USER = getUser($CFG->defaultUserID);
+	$role = getRoleByName('Idea');
+	$USER = null;
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+</head>
+
+<body>
+
+<div style="clear:both;"></div>
+<div>
+<h1>Testing Access</h1>
+<p>Role=<?php echo $role->name; ?></p>
+</div>
+
+</body>
+</html>

32b95d38b6cd97282931a6a197ec877349f11199
Michelle S Bachler <michelle.bachler@open.ac.uk>
2024-10-24 10:44:17 +0100

    New pages for new accessibility related additional text - like on iframes

---
+++ language/es/accessibility.php
@@ -0,0 +1,45 @@
+<?php
+/********************************************************************************
+ *                                                                              *
+ *  (c) Copyright 2015 - 2024 The Open University UK                            *
+ *                                                                              *
+ *  This software is freely distributed in accordance with                      *
+ *  the GNU Lesser General Public (LGPL) license, version 3 or later            *
+ *  as published by the Free Software Foundation.                               *
+ *  For details see LGPL: http://www.fsf.org/licensing/licenses/lgpl.html       *
+ *               and GPL: http://www.fsf.org/licensing/licenses/gpl-3.0.html    *
+ *                                                                              *
+ *  This software is provided by the copyright holders and contributors "as is" *
+ *  and any express or implied warranties, including, but not limited to, the   *
+ *  implied warranties of merchantability and fitness for a particular purpose  *
+ *  are disclaimed. In no event shall the copyright owner or contributors be    *
+ *  liable for any direct, indirect, incidental, special, exemplary, or         *
+ *  consequential damages (including, but not limited to, procurement of        *
+ *  substitute goods or services; loss of use, data, or profits; or business    *
+ *  interruption) however caused and on any theory of liability, whether in     *
+ *  contract, strict liability, or tort (including negligence or otherwise)     *
+ *  arising in any way out of the use of this software, even if advised of the  *
+ *  possibility of such damage.                                                 *
+ *                                                                              *
+ ********************************************************************************/
+
+// ACESSIBILITY ADDITIONS
+
+// Iframes
+$LNG->IFRAME_MAP_VIS_NETWORK = $LNG->STATS_TAB_NETWORK." Visualización";
+$LNG->IFRAME_MAP_VIS_CIRCLES = $LNG->STATS_TAB_CIRCLEPACKING." Visualización";
+$LNG->IFRAME_MAP_VIS_SUNBURST = $LNG->STATS_TAB_SUNBURST." Visualización";
+$LNG->IFRAME_MAP_VIS_TREEMAP = $LNG->STATS_TAB_TREEMAP." Visualización";
+$LNG->IFRAME_MAP_VIS_TREEMAPDT = $LNG->STATS_TAB_TREEMAPTD." Visualización";
+
+$LNG->IFRAME_MAP_ANALYTICS_OVERVIEW = $LNG->STATS_TAB_OVERVIEW." Visualización";
+$LNG->IFRAME_MAP_ANALYTICS_SOCIAL = $LNG->STATS_TAB_SOCIAL." Visualización";
+$LNG->IFRAME_MAP_ANALYTICS_RING = $LNG->STATS_TAB_RING." Visualización";
+$LNG->IFRAME_MAP_ANALYTICS_ACTIVITY = $LNG->STATS_TAB_ACTIVITY_ANALYSIS." Visualización";
+$LNG->IFRAME_MAP_ANALYTICS_USERACTIVITY = $LNG->STATS_TAB_USER_ACTIVITY_ANALYSIS." Visualización";
+$LNG->IFRAME_MAP_ANALYTICS_STREAM = $LNG->STATS_TAB_STREAMGRAPH." Visualización";
+
+$LNG->IFRAME_ABOUT_LITEMAP = "Acerca de la película LiteMap";
+$LNG->IFRAME_HELP_CREATING_ACCOUNT = "Crear una cuenta y una descripción general de la página de inicio Película";
+$LNG->IFRAME_MAP_EMBED = "Embedded LiteMap";
+$LNG->IFRAME_MAP_EMBEDEDIT = "LiteMap editable integrado";
